fc98b199b10e688bb077bdf68cdec6359c337da4
Dardila11 <danieleardila@hotmail.com>
2019-08-07 22:31:27 -0500

    messages, hud added

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -2,23 +2,16 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ScoreManager : MonoBehaviour
-{
+public class ScoreManager : MonoBehaviour {
     public static int score;
-
-
     Text text;
-
-
-    void Awake ()
-    {
-        text = GetComponent <Text> ();
+    void Awake() {
+        text = GetComponent<Text>();
         score = 0;
     }
 
 
-    void Update ()
-    {
-        text.text = "Score: " + score;
+    void Update() {
+        text.text = "Puntaje: " + score;
     }
 }
---
+++ Assets/Scripts/UIMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMessage : MonoBehaviour {
+    public GameObject message;
+    // Start is called before the first frame update
+    void Start() {
+        message.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player){
+        if(player.gameObject.tag == "Player"){
+            message.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec(){
+        yield return new WaitForSeconds(3);
+        Destroy(message);
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
